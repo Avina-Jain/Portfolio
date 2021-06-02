36553de73743ce487c63c295b029bba226df4c9e
AV <av@microsoft.com>
2021-06-03 00:45:51 +0530

    created a base, added modal and menu slider

---
+++ script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+
+//show nav
+toggle.addEventListener('click', ()=>document.body.classList.toggle('show-nav'));
+
+
+//modal pop
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+
