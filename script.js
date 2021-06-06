a10b743c669296827a6b22cdf533aff0fa1eb804
AV <av@microsoft.com>
2021-06-06 19:52:23 +0530

    close modal on outside click

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ const modal = document.getElementById('modal');
 //show nav
 toggle.addEventListener('click', ()=>document.body.classList.toggle('show-nav'));
 
-
 //modal pop
 open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+
 close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal'):false);
 
 
