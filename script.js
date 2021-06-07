a4e01e793e65dc614be09660377230c623e17ba4
AV <av@microsoft.com>
2021-06-07 11:52:39 +0530

    added left arrow icon to nav toggle button

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ const modal = document.getElementById('modal');
 
 
 //show nav
-toggle.addEventListener('click', ()=>document.body.classList.toggle('show-nav'));
+toggle.addEventListener('click', ()=>{
+    document.body.classList.toggle('show-nav');
+    const icon = document.querySelector('i');
+    if(icon.classList.contains('fa-bars')){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-arrow-left');
+    }else{
+        icon.classList.remove('fa-arrow-left');
+        icon.classList.add('fa-bars');
+        
+    }
+});
 
 //modal pop
 open.addEventListener('click', () => modal.classList.add('show-modal'));
